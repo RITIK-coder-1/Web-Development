1222456e0b04f188ff379e148ee1db02016dccce
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-04-09 20:55:10 +0530

    I understood basic create command in SQL

---
+++ MySQL/Basics/one.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Programming; -- this is a basic DDL command to create a database
+
+USE Programming; -- this is a basic DML command that is used to use a DATABASE
+
+CREATE TABLE Programming_Languages
+(
+    Name VARCHAR(10),
+    Year INT
+); -- this creates a new table with the specified attributes
+
+DESC Programming_Languages; -- a basic DDL command to show the description of a table
